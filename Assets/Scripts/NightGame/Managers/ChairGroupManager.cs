fbb377de7a4c56a5b6d472f41cb5c1ba972e5c38
RogeWood <a813712100@gmail.com>
2025-07-17 16:00:25 +0800

    feat: guest set start near door

--- Assets/Scripts/NightGame/Managers/ChairGroupManager.cs
+++ Assets/Scripts/NightGame/Managers/ChairGroupManager.cs
@@ -5,6 +5,7 @@ public class ChairGroupManager : MonoBehaviour
 {
 	[Header("Chair List")]
 	[SerializeField] private List<Transform> chairList; // 所有椅子位置
+	[SerializeField] private Transform guestEnterPoistion;
 	private HashSet<Transform> occupiedChairs = new HashSet<Transform>(); // 正在被使用的椅子集合
 
 	private void Awake()
@@ -16,6 +17,7 @@ public class ChairGroupManager : MonoBehaviour
 		{
 			chairList.Add(chairObj.transform);
 		}
+		SortChairList(); // 椅子以靠近重生點分類
 	}
 
 
@@ -27,29 +29,45 @@ public class ChairGroupManager : MonoBehaviour
 		}
 	}
 
-	/// 隨機尋找一個空的椅子並標記為已佔用。找不到則回傳 null。
+	///// 隨機尋找一個空的椅子並標記為已佔用。找不到則回傳 null。
+	//public Transform FindEmptyChair()
+	//{
+	//	// 建立一個清單來存放所有尚未被佔用的椅子
+	//	List<Transform> availableChairs = new List<Transform>();
+
+	//	foreach (Transform chair in chairList)
+	//	{
+	//		if (!occupiedChairs.Contains(chair))
+	//			availableChairs.Add(chair);
+	//	}
+
+	//	// 若沒有空椅子，回傳 null
+	//	if (availableChairs.Count == 0)
+	//		return null;
+
+	//	// 隨機選取其中一張椅子
+	//	Transform selectedChair = availableChairs[Random.Range(0, availableChairs.Count)];
+
+	//	// 標記為已佔用
+	//	occupiedChairs.Add(selectedChair);
+
+	//	return selectedChair;
+	//}
+
+	/// 依序尋找一個空的椅子並標記為已佔用。找不到則回傳 null。
 	public Transform FindEmptyChair()
 	{
-		// 建立一個清單來存放所有尚未被佔用的椅子
-		List<Transform> availableChairs = new List<Transform>();
-
 		foreach (Transform chair in chairList)
 		{
 			if (!occupiedChairs.Contains(chair))
-				availableChairs.Add(chair);
+			{
+				occupiedChairs.Add(chair);
+				return chair;
+			}
 		}
 
 		// 若沒有空椅子，回傳 null
-		if (availableChairs.Count == 0)
-			return null;
-
-		// 隨機選取其中一張椅子
-		Transform selectedChair = availableChairs[Random.Range(0, availableChairs.Count)];
-
-		// 標記為已佔用
-		occupiedChairs.Add(selectedChair);
-
-		return selectedChair;
+		return null;
 	}
 
 	/// 當客人離席時釋放椅子。
@@ -95,5 +113,14 @@ public class ChairGroupManager : MonoBehaviour
 	public bool IsChairccupied(Transform chair)
 	{
 		return occupiedChairs.Contains(chair);
+	}
+
+	private void SortChairList()
+	{
+		// 按照距離 spawnPoistion 的遠近排序（由近到遠）
+		chairList.Sort((a, b) =>
+			Vector3.Distance(a.position, guestEnterPoistion.position)
+			.CompareTo(Vector3.Distance(b.position, guestEnterPoistion.position))
+		);
 	}
 }
