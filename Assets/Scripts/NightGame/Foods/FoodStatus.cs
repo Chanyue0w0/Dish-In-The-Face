76cf291876032272489903dee480482f442634f0
RogeWood <a813712100@gmail.com>
2025-08-14 17:55:19 +0800

    feat: new slide code

--- Assets/Scripts/NightGame/Foods/FoodStatus.cs
+++ Assets/Scripts/NightGame/Foods/FoodStatus.cs
@@ -1,10 +1,10 @@
 //using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
-
+using FoodsGroup;
 public class FoodStatus : MonoBehaviour
 {
-    [SerializeField] public FoodsGroupManager.FoodType foodType;
+    [SerializeField] public FoodType foodType;
     //// Start is called before the first frame update
     //void Start()
     //{
