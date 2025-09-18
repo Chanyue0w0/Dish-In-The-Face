ed48a937588c407e0104827c956c3cf9c5525814
RogeWood <a813712100@gmail.com>
2025-09-18 22:20:05 +0800

    feat: attack open door

--- Assets/Scripts/NightGame/Furnitures/ForceLeaveDoor.cs
+++ Assets/Scripts/NightGame/Furnitures/ForceLeaveDoor.cs
@@ -3,13 +3,25 @@ using UnityEngine;
 public class ForceLeaveDoor : MonoBehaviour
 {
     private static readonly int IsOpen = Animator.StringToHash("isOpen");
+    [SerializeField] private bool attackToOpen = false;
     [SerializeField] private Vector2 outDirection;
     [SerializeField] private Animator animator;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] private string stunTriggerTag = "AttackStun"; // 造成暈眩的攻擊 Trigger Tag
+    [SerializeField] private string playerTag = "Player";
+    private void OnTriggerStay2D(Collider2D other)
     {
+        if (!other) return;
+        
         BeForceOut otherOut = other.GetComponent<BeForceOut>();
-        if (other.CompareTag("Player") || otherOut != null)
+        
+        // 碰撞判斷
+        if (attackToOpen && other.CompareTag(stunTriggerTag))
+        {
+            if (otherOut != null) otherOut.SetDirection(outDirection);
+            animator.SetBool(IsOpen, true);
+        }
+        else if (!attackToOpen && other.CompareTag(playerTag))
         {
             if (otherOut != null) otherOut.SetDirection(outDirection);
             animator.SetBool(IsOpen, true);
@@ -24,4 +36,6 @@ public class ForceLeaveDoor : MonoBehaviour
             animator.SetBool(IsOpen, false);
         }
     }
+    
+    public bool IsDoorOpen() => animator.GetBool(IsOpen);
 }
